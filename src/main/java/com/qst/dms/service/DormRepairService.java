23b7e62dedc92bf2da67d5e772ababf94cf77fe4
sve1r <plekiyeon@gmail.com>
2020-11-10 19:05:47 +0800

    初始化仓库

---
+++ src/main/java/com/qst/dms/service/DormRepairService.java
@@ -0,0 +1,26 @@
+package com.qst.dms.service;
+
+
+
+import com.qst.dms.po.DormRepair;
+import com.qst.dms.po.PageInfo;
+
+import java.util.List;
+
+/**
+ * @program: dormitorySystem
+ * @description: 维修登记
+ * @author: Joyrocky
+ * @create: 2019-04-28 00:25
+ **/
+public interface DormRepairService {
+
+    //分页查询
+    public PageInfo<DormRepair> findPageInfo(Integer d_id, String d_dormbuilding, Integer pageIndex, Integer pageSize);
+
+    public int addDormRepair(DormRepair dormrepair);    //添加宿舍信息
+    public int deleteDormRepair(Integer r_id);   //删除宿舍信息
+    public int updateDormRepair(DormRepair dormrepair); //修改宿舍信息
+    public DormRepair findDormRepairById(Integer r_id);
+    public List<DormRepair> getAll();
+}
